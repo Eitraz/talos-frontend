c6123c20432bfa44e10ebd968dfac94833483f87
Eitraz <git@eitraz.com>
2017-11-21 21:48:50 +0100

    Trying out form dialog

--- src/main/java/com/eitraz/talos/frontend/view/DashboardView.java
+++ src/main/java/com/eitraz/talos/frontend/view/DashboardView.java
@@ -1,5 +1,6 @@
 package com.eitraz.talos.frontend.view;
 
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
@@ -119,12 +120,70 @@ public class DashboardView extends HorizontalLayout implements View {
     private Component createAddNewPanel() {
         Button button = new Button(VaadinIcons.PLUS_CIRCLE_O);
         button.addStyleName(ValoTheme.BUTTON_ICON_ONLY);
-        button.addStyleName(ValoTheme.BUTTON_PRIMARY);
-        button.addStyleName("dashed");
+        button.addStyleName(ValoTheme.BUTTON_FRIENDLY);
+        button.addStyleName("bright");
         button.setWidth(100, Unit.PERCENTAGE);
         button.setHeight(50, Unit.PIXELS);
+
+        button.addClickListener(e -> new AddWindow().open());
 
         return button;
     }
 
+    private class AddWindow extends Window {
+        public AddWindow() {
+            super();
+
+            addCloseShortcut(ShortcutAction.KeyCode.ESCAPE, null);
+
+            setModal(true);
+            setClosable(false);
+            setResizable(false);
+
+            FormLayout form = new FormLayout();
+            {
+                Label section = new Label("Lorem Ipsum");
+                section.addStyleNames(ValoTheme.LABEL_H3, ValoTheme.LABEL_COLORED);
+                form.addComponent(section);
+
+                form.addComponent(new TextField("Name"));
+
+                form.setSizeUndefined();
+                form.setMargin(false);
+            }
+
+            Panel panel = new Panel(new VerticalLayout(form));
+            {
+                panel.addStyleName(ValoTheme.PANEL_BORDERLESS);
+                panel.addStyleName(ValoTheme.PANEL_SCROLL_INDICATOR);
+            }
+
+            HorizontalLayout footer = new HorizontalLayout();
+            {
+                Label footerText = new Label("");
+                footerText.setSizeUndefined();
+
+                Button ok = new Button("OK", event -> close());
+                ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
+
+                Button cancel = new Button("Close", event -> close());
+
+                footer.addStyleName(ValoTheme.WINDOW_BOTTOM_TOOLBAR);
+                footer.setWidth(100, Unit.PERCENTAGE);
+                footer.addComponents(footerText, ok, cancel);
+                footer.setExpandRatio(footerText, 1);
+            }
+
+
+            VerticalLayout content = new VerticalLayout(panel, footer);
+            content.setExpandRatio(panel, 1);
+            setContent(content);
+        }
+
+        public void open() {
+            DashboardView.this.getUI().addWindow(this);
+            center();
+            focus();
+        }
+    }
 }
